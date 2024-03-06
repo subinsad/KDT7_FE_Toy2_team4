1ac6f10064daaa1ad3aaa0f2c65b27bf80a76b80
subbi <92204014+subinsad@users.noreply.github.com>
2024-03-06 23:21:30 +0900

    근태페이지 권한부여

--- src/pages/Attendance.jsx
+++ src/pages/Attendance.jsx
@@ -1,26 +1,32 @@
-import React from "react";
-import Card from "../components/Card";
-import BoardList from "../components/Board/BoardList";
-import { Button } from "../components/GlobalStyles";
-import { useNavigate } from "react-router-dom";
+import React, { useState } from 'react';
+import Card from '../components/Card';
+import BoardList from '../components/Board/BoardList';
+import { Button } from '../components/GlobalStyles';
+import { useNavigate } from 'react-router-dom';
 
 const Attendance = () => {
-  const navigate = useNavigate();
-  const gotoWrite = () => {
-    navigate("/attendance/write");
-  };
-  return (
-    <div>
-      <Card title={"Member Salary List"}>
-        <div className="align right mb3">
-          <Button $color="primary" onClick={gotoWrite}>
-            근태신청
-          </Button>
+    const navigate = useNavigate();
+    const gotoWrite = () => {
+        navigate('/attendance/write');
+    };
+
+    const [isAdmin, setIsAdmin] = useState(false);
+    const [currentUser, setCurrentUser] = useState({ id: 'user1' });
+
+    return (
+        <div>
+            <Card title={'Member Salary List'}>
+                <div className="align right mb3">
+                    {currentUser && (
+                        <Button $color="primary" onClick={gotoWrite}>
+                            근태신청
+                        </Button>
+                    )}
+                </div>
+                <BoardList state={'attendance'} />
+            </Card>
         </div>
-        <BoardList state={"attendance"} />
-      </Card>
-    </div>
-  );
+    );
 };
 
 export default Attendance;
