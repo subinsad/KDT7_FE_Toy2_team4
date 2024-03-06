23d166e0b7f2589e5e632037da3e408aeadcbbd7
subbi <92204014+subinsad@users.noreply.github.com>
2024-03-06 23:20:40 +0900

    리스트페이지

--- src/components/Board/AttendanceList.jsx
+++ src/components/Board/AttendanceList.jsx
@@ -1,130 +1,128 @@
-import React from "react";
-import { Badge, Button, Grid, GridColumnSpan, PagingItem, Table, TablePaging } from "../GlobalStyles";
-import Dialog from "../Dialog";
-import Input from "../Input";
-import Radio, { RadioGroup } from "../Radio";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import {
+    Badge,
+    Button,
+    Grid,
+    GridColumnSpan,
+    PagingItem,
+    Table,
+    TablePaging,
+} from '../GlobalStyles';
+import Dialog from '../Dialog';
+import Input from '../Input';
+import Radio, { RadioGroup } from '../Radio';
+import { Link } from 'react-router-dom';
+import AttendanceListItem from './AttendanceListItem';
 
 const AttendanceList = ({ ...props }) => {
-  return (
-    <>
-      <Table {...props}>
-        <colgroup>
-          <col />
-          <col style={{ width: "220px" }} />
-          <col style={{ width: "150px" }} />
-          <col style={{ width: "150px" }} />
-          <col style={{ width: "100px" }} />
-        </colgroup>
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Date</th>
-            <th>Category</th>
-            <th>Status</th>
-            <th>Setting</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>
-              <Link to="/attendance/read">휴가입니다.</Link>
-            </td>
-            <td>2024.02.20 ~ 2024.03.20</td>
-            <td>
-              <Badge $color="primary">휴가</Badge>
-            </td>
-            <td>
-              <Badge $color="success">승인</Badge>
-            </td>
-            <td>
-              <Button $color="primary" $size="xs" popovertarget="settings">
-                설정
-              </Button>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <Link to="/attendance/read">신청합니다.</Link>
-            </td>
-            <td>2024.02.20</td>
-            <td>
-              <Badge $color="warning">조퇴</Badge>
-            </td>
-            <td>
-              <Badge $color="primary">대기</Badge>
-            </td>
-            <td>
-              <Button $color="primary" $size="xs" popovertarget="settings">
-                설정
-              </Button>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <Link to="/attendance/read">신청합니다.</Link>
-            </td>
-            <td>2024.02.20 ~ 2024.03.20</td>
-            <td>
-              <Badge $color="danger">경조사</Badge>
-            </td>
-            <td>
-              <Badge $color="danger">반려</Badge>
-            </td>
-            <td>
-              <Button $color="primary" $size="xs" popovertarget="settings">
-                설정
-              </Button>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <Link to="/attendance/read">신청합니다.</Link>
-            </td>
-            <td>2024.02.20 ~ 2024.03.20</td>
-            <td>
-              <Badge $color="secondary">기타</Badge>
-            </td>
-            <td>
-              <Badge $color="danger">반려</Badge>
-            </td>
-            <td>
-              <Button $color="primary" $size="xs" popovertarget="settings">
-                설정
-              </Button>
-            </td>
-          </tr>
-        </tbody>
-      </Table>
-      <TablePaging>
-        <PagingItem>Previous</PagingItem>
-        <PagingItem $active>1</PagingItem>
-        <PagingItem>2</PagingItem>
-        <PagingItem>3</PagingItem>
-        <PagingItem>4</PagingItem>
-        <PagingItem>5</PagingItem>
-        <PagingItem>Next</PagingItem>
-      </TablePaging>
+    const [attendances, setAttendances] = useState([]); // json
+    const [isAdmin, setIsAdmin] = useState(false);
+    const [currentUser, setCurrentUser] = useState({ id: 'user1' }); // 로그인한 사용자 정보
 
-      <Dialog id={"settings"}>
-        <div className="text-left">
-          <RadioGroup title="Category">
-            <Radio value="승인" checked={true} id="settingRadio1" name="settingRadio" color="success" />
-            <Radio value="대기" id="settingRadio2" name="settingRadio" color="primary" />
-            <Radio value="반려" id="settingRadio3" name="settingRadio" color="danger" />
-          </RadioGroup>
-        </div>
-        <div>
-          <Button $color="success" $size="sm" popovertarget="settings" popovertargetaction="hide" className="mr2">
-            설정
-          </Button>
-          <Button $color="secondary" $size="sm" popovertarget="settings" popovertargetaction="hide">
-            닫기
-          </Button>
-        </div>
-      </Dialog>
-    </>
-  );
+    useEffect(() => {
+        fetch('http://localhost:3000/attendance')
+            .then((res) => {
+                return res.json();
+            })
+            .then((data) => {
+                setAttendances(data || []);
+            })
+            .catch((err) => {
+                console.error(err);
+            });
+
+        setIsAdmin(false); //임시로 관리자는 안보이게
+
+        setCurrentUser({ id: 'user1' }); // 현재사용자정보
+    }, []);
+    return (
+        <>
+            <Table {...props}>
+                <colgroup>
+                    <col />
+                    <col style={{ width: '220px' }} />
+                    <col style={{ width: '150px' }} />
+                    <col style={{ width: '150px' }} />
+                    <col style={{ width: '100px' }} />
+                </colgroup>
+                <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Date</th>
+                        <th>Category</th>
+                        <th>Status</th>
+                        {isAdmin && <th>Setting</th>}
+                    </tr>
+                </thead>
+                <tbody>
+                    {/* 아이템 map으로 배열 */}
+                    {attendances.map((item) => {
+                        if (item.authorId === currentUser?.id || isAdmin) {
+                            return (
+                                <tr key={item.id}>
+                                    <AttendanceListItem item={item} />
+                                </tr>
+                            );
+                        } else {
+                            return null;
+                        }
+                    })}
+                </tbody>
+            </Table>
+            <TablePaging>
+                <PagingItem>Previous</PagingItem>
+                <PagingItem $active>1</PagingItem>
+                <PagingItem>2</PagingItem>
+                <PagingItem>3</PagingItem>
+                <PagingItem>4</PagingItem>
+                <PagingItem>5</PagingItem>
+                <PagingItem>Next</PagingItem>
+            </TablePaging>
+
+            <Dialog id={'settings'}>
+                <div className="text-left">
+                    <RadioGroup title="Category">
+                        <Radio
+                            value="승인"
+                            checked={true}
+                            id="settingRadio1"
+                            name="settingRadio"
+                            color="success"
+                        />
+                        <Radio
+                            value="대기"
+                            id="settingRadio2"
+                            name="settingRadio"
+                            color="primary"
+                        />
+                        <Radio
+                            value="반려"
+                            id="settingRadio3"
+                            name="settingRadio"
+                            color="danger"
+                        />
+                    </RadioGroup>
+                </div>
+                <div>
+                    <Button
+                        $color="success"
+                        $size="sm"
+                        popovertarget="settings"
+                        popovertargetaction="hide"
+                        className="mr2">
+                        설정
+                    </Button>
+                    <Button
+                        $color="secondary"
+                        $size="sm"
+                        popovertarget="settings"
+                        popovertargetaction="hide">
+                        닫기
+                    </Button>
+                </div>
+            </Dialog>
+        </>
+    );
 };
 
 export default AttendanceList;
